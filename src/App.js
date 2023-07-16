a6c800e11819f6aa78c859158abda7dd98ad01dc
Pamela Zambrana <p_zf888@hotmail.com>
2023-07-15 20:15:37 -0400

    SDeleting some useless files

--- src/App.js
+++ src/App.js
@@ -11,12 +11,6 @@ import LandingServicesPage from "./pages/public/landing/LandingServicesPage";
 import LandingSpecialistPage from "./pages/public/landing/LandingSpecialistsPage";
 import LandingContactsPage from "./pages/public/landing/LandingContactsPage";
 import LoginPage from './pages/public/LoginPage';
-import AppointmentTablePage from "./pages/Private/Tables/AppointmentTablePage";
-import WorkersTablePage from "./pages/Private/Tables/WorkersTablePage";
-import PatientsTablePage from './pages/Private/Tables/PatientsTablePage';
-import NewWorkerFormPage from './pages/Private/Forms/NewWorkerFormPage';
-import NewPatientFormPage from './pages/Private/Forms/NewPatientFormPage';
-import NewAppointmentFormPage from './pages/Private/Forms/NewAppointmentFormPage';
 import GlobalProvider from './globalContext/globalContext';
 
 
@@ -28,14 +22,6 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path="especialistas" element=<LandingSpecialistPage/>/>
     <Route path="contactos" element=<LandingContactsPage/>/>
     <Route path='login' element=<LoginPage/>/>
-    <Route path="private">
-      <Route index element=<AppointmentTablePage/>></Route>
-      <Route path ='trabajadores' element=<WorkersTablePage/>></Route>
-      <Route path ="pacientes" element=<PatientsTablePage/>></Route>
-      <Route path='nuevo-trabajador' element=<NewWorkerFormPage/>></Route>
-      <Route path='nuevo-paciente' element=<NewPatientFormPage/>></Route>
-      <Route path='nueva-cita' element=<NewAppointmentFormPage/>></Route>
-    </Route>
   </Route>
 ))
 
